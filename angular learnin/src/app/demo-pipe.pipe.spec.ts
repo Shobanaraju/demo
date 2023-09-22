f6a1f5dacc0bc8ccb80e9c3456051dc6ddad3e42
Shobana R <shobana.r@anniyam.com>
2023-09-22 10:17:19 +0530

    student application

--- angular learnin/src/app/demo-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DemoPipePipe } from './demo-pipe.pipe';
-
-describe('DemoPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DemoPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
